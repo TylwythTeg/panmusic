b0f4cece6a9420d27176198c81428d84d043a684
Rob <garciarobertoiv@yahoo.com>
2017-02-12 16:08:27 -0800

    Added links for triads on Scale pages

--- webapp/urls_scales.py
+++ webapp/urls_scales.py
@@ -4,6 +4,7 @@ from Scale import *
 
 urlpatterns = [
     url(r'^$', views.index_scales, name='index_scales'),
-    url(r'^(?P<scale_type>[\w|\W]+)/', views.scale, name = 'scale')
+    url(r'^(?P<scale_type>[\w|\W]+)/', views.scale, name = 'scale'),
+    #url(r'^(?P<chord_type>[\w|\W]+)/$', views.chord, name = 'chord')
     
 ]
