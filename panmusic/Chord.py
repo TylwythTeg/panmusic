6aa75e2e5c8d22a216ee06fcced35792d0137168
TylwythTeg <garciarobertoiv@yahoo.com>
2017-05-15 20:25:49 -0700

    cleaned u pcode

--- panmusic/Chord.py
+++ panmusic/Chord.py
@@ -4,7 +4,7 @@ from enum import Enum
 class Chord():
     name = None
     notes = []
-    _tetrad = None
+    tetrad = None
     triad = None
     
 
@@ -16,72 +16,7 @@ class Chord():
         self.notes = [self.root]
         for interval in self.intervals:
             self.notes.append(self.root.plus(interval))
-    
-    def old_create(type, root, triad = "None"):
-        types = {
-            "Major": MajorTriad(root),
-            "Minor": MinorTriad(root),
-            "Suspended Two": SuspendedTriad(root, Interval.MAJOR_SECOND),
-            "Suspended Four": SuspendedTriad(root, Interval.FOURTH),
-            "Augmented": AugmentedTriad(root),
-            "Diminished": DiminishedTriad(root),
-            "Flat Five": FlatFiveTriad(root),
 
-            "Dominant Seven": DominantSeventhChord(triad,root),
-            "Major Seven": MajorSeventhChord(triad,root),
-            "Diminished Seven": DiminishedSeventhChord(triad,root),
-
-            #"All": AllChords(root)
-        }
-        return types.get(type)
-
-    def tetrad_setter(self, tetrad):
-        self._tetrad = tetrad
-
-    def tetrad_getter(self):
-        return self._tetrad
-
-    tetrad = property(tetrad_getter, tetrad_setter)
-
-
-
-
-    def triad_setter(self, triad):
-        self.triad = triad
-        print("-------------------------",triad)
-        '''
-        #If no tetrad in Chord, use the triad string as the class
-        if self.tetrad is None:
-
-            #check if object instead of string and get string
-            if isinstance(triad,Triad):
-                triad = triad.type
-
-            #update() can use self.update but would go through all checks
-            #update(triad) 
-            triad = triad.__str__()
-            self.__class__ = globals()[triad + "Triad"]
-            self.type = triad
-            self.name = self.root.__str__() + " " + self.type
-        else:
-
-            tetrad_type = self.tetrad_type.replace(" ","")
-
-            self.__class__ = globals()[tetrad_type + "Chord"]
-            print("CLASS IS :",self.__class__)
-            #will want to do based on type
-            self. type = tetrad
-            self.name = self.root__str_() + "" + self.type
-            #update class by 7
-
-        print("CASDD",self.__class__)
-        #self._class_ = Triad.classes['triad']
-
-    def triad_getter(self):
-        return self._triad
-
-    triad = property(triad_getter,triad_setter)
-    '''
 
 
 
@@ -91,57 +26,11 @@ class Chord():
     #   triad
     #   tetrad
     def create(root = None, triad = None, tetrad = None ):
-        #no tetrad, so go down one
-        print(root, triad, tetrad,"-----------------")
 
         if tetrad is None:
-            return Triad.zcreate(root, triad)
-        #creating a tetrad
-        else: 
-            #print("I got a this chord///////////////////////////////")
-            #return Chord.old_create(tetrad, root, triad)
-            #return Tetrad.create(root =x_root, triad =x_triad, tetrad = x_tetrad)
-            #print(root, triad, tetrad)
-            
-            return Tetrad.create(tetrad, triad, root)
-
-
-
-    def kcreate(**chord):
-        for k,v in chord.items():
-            print(k,v)
-
-        #no tetrad, so go down one
-        if 'tetrad' not in chord:
-            return Triad.kcreate(**chord)
-        else:
-            return Chord.create(chord['tetrad'], chord['root'], chord['triad'])
-
-
-
-            #triad = chord["triad"]
-            #constructor = Triad.constructors[triad]
-            #return constructor(chord["root"])
-
-        #constructor = Triad.constructors.get(chord["triad"]):
-
-
-        '''
-        triads = {
-            "Major" = MajorTriad(root),
-            "Minor": = MinorTriad(root),
-            "Suspended Two": SuspendedTriad(root, Interval.MAJOR_SECOND),
-            "Suspended Two": SuspendedTriad(root, Interval.MAJOR_SECOND),
-            "Augmented": AugmentedTriad(root),
-            "Diminished": DiminishedTriad(root),
-            "Flat Five": FlatFifthTriad(root),
-        }
-        '''
-
-
-
-        #if 'tetrad' not in kwargs:
-            #for k,v in kwargs.items():
+            return Triad.create(root = root, triad = triad)
+        else:    
+            return Tetrad.create(tetrad = tetrad,triad = triad,root = root)
 
 
 
@@ -170,85 +59,15 @@ class Triad(Chord):
     ]
 
     def constructors(triad_type):
-        #for triad in types:
-        print("CHECKECKECKECKECKEKECKECKECKECKECKECKECKECKECKECKECKEKCECKCEK")
         return globals()[triad_type+"Triad"]
-
-        triads = {
-            "Major": MajorTriad(root),
-            "Minor": MinorTriad(root),
-            "Suspended Two": SuspendedTriad(root, Interval.MAJOR_SECOND),
-            "Suspended Two": SuspendedTriad(root, Interval.MAJOR_SECOND),
-            "Augmented": AugmentedTriad(root),
-            "Diminished": DiminishedTriad(root),
-            "Flat Five": FlatFiveTriad(root),
-        }
-        return triads.get(triad_type,"what")
     
 
-    def create(triad, root):
-        types = {
-            "Major": MajorTriad(root),
-            "Minor": MinorTriad(root),
-            "Suspended Two": SuspendedTriad(root, Interval.MAJOR_SECOND),
-            "Suspended Four": SuspendedTriad(root, Interval.FOURTH),
-            "Augmented": AugmentedTriad(root),
-            "Diminished": DiminishedTriad(root),
-            "Flat Five": FlatFiveTriad(root)
-            }
-        return types.get(triad)
-
-
-    def zcreate(root = None, triad = None ):
-
+    def create(root = None, triad = None ):
         constructor = Triad.constructors(triad)
         return constructor(root)
 
-
-    def kcreate(**chord):
-        triad = chord["triad"]
-        print("THIS THING RAR",chord["triad"])
-        constructor = Triad.constructors(triad)
-        return constructor(chord["root"])
-    
-    
-
-
     def __str__(self):
         return self.name
-
-class Tetrad(Chord):
-    types = [
-        "Seven",
-        "Six",
-    ]
-
-    def constructors(tetrad_type):
-        #for triad in types:
-        '''
-        return globals()[triad_type+"Triad"]
-
-        tetrads = {
-            "Major": MajorTriad(root),
-            "Minor": MinorTriad(root),
-        }
-        return tetrads.get(triad_type,"what")
-        '''
-
-        tetrad_type = tetrad_type.replace(" ", "")
-        return globals()[tetrad_type + "thChord"]
-
-    def create(name, triad, root):
-        print("hey")
-       #types = {
-        #    "Dominant Seven": DominantSeventhChord(triad,root),
-         #   "Major Seven": MajorSeventhChord(triad,root),
-          #  "Diminished Seven": DiminishedSeventhChord(triad,root),
-           # }
-        #return types.get(name)
-
-        constructor = Tetrad.constructors(name)
-        return constructor(triad, root)
 
 
 
@@ -337,6 +156,21 @@ class FlatFiveTriad(Triad):
   
 
 
+class Tetrad(Chord):
+    types = [
+        "Seven",
+        "Six",
+    ]
+
+    def constructors(tetrad_type):
+        tetrad_type = tetrad_type.replace(" ", "")
+        return globals()[tetrad_type + "thChord"]
+
+    def create(tetrad = None, triad = None, root = None):
+        constructor = Tetrad.constructors(tetrad)
+        return constructor(triad, root)
+
+
 class SeventhChord(Tetrad):
     types = [
         "Dominant Seven",
@@ -373,7 +207,10 @@ class DominantSeventhChord(SeventhChord):
         print(self.name)
         self.root = root
         self.tetrad_type = "Dominant Seventh"
-        self.triad = Triad.create(triad, root)
+        self.triad = Triad.create(
+                    triad = triad,
+                    root = root
+                    )
 
         #t_intervals = self.intervals
         #t_triad_intervals = self.triad.intervals
@@ -401,9 +238,10 @@ class MajorSeventhChord(SeventhChord):
         self.name = root.__str__() + " " + self.type
         self.root = root
         self.tetrad_type = "Major Seventh"
-        self.triad = Triad.create(triad, root)
-        #print("0intervals:", self.intervals)
-        #print("triad_intervals:", self.triad.intervals)
+        self.triad = Triad.create(
+                    triad = triad,
+                    root = root
+                    )
         self.intervals = self.triad.intervals + self.seventh_interval
         #print("intervals:", self.intervals)
         
@@ -426,7 +264,10 @@ class DiminishedSeventhChord(SeventhChord):
         self.name = root.__str__() + " " + self.type
         self.root = root
         self.tetrad_type = "Diminished Seventh"
-        self.triad = Triad.create(triad, root)
+        self.triad = Triad.create(
+                    triad = triad,
+                    root = root
+                    )
         self.intervals = self.triad.intervals + self.seventh_interval
         
         self.generate_notes()
